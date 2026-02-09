b7d8b2ebdd0b4959cdc860c6e8d90b76912cfee3
Dzianis Shapel <dshapel@alfabank.ru>
2026-02-09 20:44:00 +0300

    push

--- src/test/java/AuthentificationAPI/LoginApiTest.java
+++ src/test/java/AuthentificationAPI/LoginApiTest.java
@@ -9,8 +9,10 @@ public class LoginApiTest extends BaseTest {
   protected static final String dashboard_url = Config.get("dashboard.url");
 
   @Test
-  public void loginApiTest() {
+  public void loginApiTest() throws InterruptedException {
     LoginAPI.loginWithCookies(driver);
+    Thread.sleep(5000);
     driver.get(dashboard_url);
+    Thread.sleep(5000);
   }
 }
